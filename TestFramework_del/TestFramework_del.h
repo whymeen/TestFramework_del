5815a329c1bcc706a942e20642a2133e2983b6ab
whymeen <whymeen@gmail.com>
2016-12-02 15:37:35 +0900

    초기 커밋.

---
+++ TestFramework_del/TestFramework_del.h
@@ -0,0 +1,32 @@
+
+// TestFramework_del.h : PROJECT_NAME 응용 프로그램에 대한 주 헤더 파일입니다.
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH에 대해 이 파일을 포함하기 전에 'stdafx.h'를 포함합니다."
+#endif
+
+#include "resource.h"		// 주 기호입니다.
+
+
+// CTestFramework_delApp:
+// 이 클래스의 구현에 대해서는 TestFramework_del.cpp을 참조하십시오.
+//
+
+class CTestFramework_delApp : public CWinApp
+{
+public:
+	CTestFramework_delApp();
+
+// 재정의입니다.
+public:
+	virtual BOOL InitInstance();
+
+// 구현입니다.
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CTestFramework_delApp theApp;
